0b593fb6efecf40b4b7a40c0de17022f25357cdd
Akshitha Kodimela <axk230162@utdallas.edu>
2026-04-20 18:35:25 -0500

    screen set up, defining the roll, defining the outcome

--- src/dice_roll_visual.py
+++ src/dice_roll_visual.py
@@ -1,3 +1,21 @@
 import pygame
 import random
 import time
+
+pygame.init()
+
+# screen set up
+width, height = 500, 400
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Roll for Fate")
+
+font = pygame.font.Font(None, 60)
+small_font = pygame.font.Font(None, 40)
+
+def roll_d20():
+    return random.randint(1,20)
+
+def get_outcome(roll):
+    
+
+pygame.quit()
