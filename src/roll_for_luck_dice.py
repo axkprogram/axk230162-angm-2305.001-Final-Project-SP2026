e29e7a6e6af784afcaa38b61adcbd724c42f6612
Akshitha Kodimela <axk230162@utdallas.edu>
2026-04-20 18:18:27 -0500

    if, else, elif statements for the success based on the numbers

--- src/roll_for_luck_dice.py
+++ src/roll_for_luck_dice.py
@@ -5,3 +5,17 @@ def roll_d20():
 
 def story_roll():
     roll = roll_d20()
+
+    if roll == 1:
+        outcome = "critical fail"
+    elif roll < 8:
+        outcome = "fail"
+    elif roll <= 12:
+        outcome = "average"
+    elif roll < 20:
+        outcome = "success"
+    else: # roll == 20
+        outcome = "critical success"
+
+    return roll, outcome
+
