84f70da0441898e683e3be8016fd70422a5a997d
Akshitha Kodimela <axk230162@utdallas.edu>
2026-04-20 20:49:38 -0500

    defining variables and finishing button

--- src/dice_roll_visual.py
+++ src/dice_roll_visual.py
@@ -21,3 +21,30 @@ def update_dice():
         fake_roll = random.randint(1,20)
         canvas.itemconfig(dice_text, text=str(fake_roll))
         root.after(50, update_dice)
+
+def roll_animation():
+    """Start rolling animation, then settle on the final result"""
+    global rolling
+    rolling = True
+
+    canvas.itemconfig(outcome_text, text="Rolling...")
+
+    update_dice()
+
+    # stop after 1.5 seconds
+    root.after(1500, stop_roll)
+
+    def stop_roll():
+        global rolling
+        rolling = False
+
+        roll, outcome = stop_roll()
+
+        canvas.itemconfig(dice_text, text+str(roll))
+        canvas.itemconfig(outcome_text, text=outcome.capitalize())
+
+# button
+btn = tk.Button(root, text="Roll D20", command= roll_animation, font=("Arial", 14))
+btn.pacl(pady=10)
+
+root.mainloop()
