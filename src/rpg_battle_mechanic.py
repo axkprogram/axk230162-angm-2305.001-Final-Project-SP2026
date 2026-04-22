ac539be31f9bc65ec5b35643020d08a7a05e14be
Akshitha Kodimela <axk230162@utdallas.edu>
2026-04-21 21:29:48 -0500

    class character defining

--- src/rpg_battle_mechanic.py
+++ src/rpg_battle_mechanic.py
@@ -1,3 +1,15 @@
 import random
 from roll_for_luck_dice import story_roll
 from combat_roll import roll_damage
+
+class Character:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        print(f"{self.name} takes {dmg} damage! (HP: {self.hp})")
+
+    def is_alive(self):
+        return self.hp > 0
