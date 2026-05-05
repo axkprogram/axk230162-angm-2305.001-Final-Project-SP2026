a3f953fb7c764e949cca412435f36c4ca16a4307
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-05 15:39:35 -0500

    establishing a general tree, not the true one, just testing a way to code a tree

--- src/story_tree.py
+++ src/story_tree.py
@@ -2,4 +2,22 @@ class StoryNode:
     def __init__(self, text, choices=None):
         self.text = text
         self.choices = choices or []
-        
+
+class Choice:
+    def __init__(self, text, next_node):
+        self.text = text
+        self.next_node = next_node
+
+# Ending nodes
+ending_good = StoryNode("You saved the village. The end.")
+ending_bad = StoryNode("The village was destroyed. The end.")
+
+# Branches
+fight = StoryNode("You chose to fight!", [Choice ("keep fighting", ending_good), Choice("Run away", ending_bad)])
+
+run = StoryNode("You ran away safely. The end.")
+
+# Start Node
+start = StoryNode("A dragon attacks your vilalge!", [
+    Choice("fight the dragon", fight), Choice("Run away", run)
+])
