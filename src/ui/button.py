e2206b1d23ef4a64e0531a2baa679ceefd222a18
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-05 18:15:43 -0500

    defining the draw for the button shape

--- src/ui/button.py
+++ src/ui/button.py
@@ -5,3 +5,8 @@ class Button:
         self.rect = pygame.Rect(rect)
         self.text = text
         self.action = action
+
+    def draw(self, screen, font):
+        pygame.draw.rect(screen, (100,100,100), self.rect)
+        txt = font.render(self.text, True (255,255,255))
+        screen.blit(txt, (self.rect.x + 10,self.rect.y + 10))
