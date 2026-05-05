eb5a64bd9e737c8bcefe33bc3658ebea8c2c2a09
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-05 18:57:54 -0500

    renamed state.py to game_state.py

--- src/engine/game_state.py
+++ src/engine/game_state.py
@@ -1,15 +1,20 @@
 class GameState:
     def __init__(self):
-        self.player_hp = 100
-        self.fighter_hp = 120
-        self.mage_hp = 80
+        self.flags = []
 
         self.fighter_favor = 0
         self.mage_favor = 0
 
-        self.flags = {}
+        self.player_hp = 100
 
-        self.inventory = []
+        self.combat_result = None
+        self.in_combat = False
+
+    def set_flag (self, key, value=True):
+        self.flags[key] = value
+
+    def get_flag(self, key):
+        return self.flags.get(key, False)
 
     def modify_favor(self, character, amount):
         if character == "fighter":
