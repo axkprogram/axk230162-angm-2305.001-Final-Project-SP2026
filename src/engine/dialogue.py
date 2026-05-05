e466f096b27a52fe9c720bd3ef0f4bcf986dc5b8
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-05 18:09:08 -0500

    defining the update text in the dialogue manager class

--- src/engine/dialogue.py
+++ src/engine/dialogue.py
@@ -13,3 +13,12 @@ class DialogueManager:
         self.displayed = ""
         self.index = 0
         self.done = False
+
+    def update(self):
+        if not self.done:
+            self.index += 1
+            self.displayed = self.text[:self.index]
+            if self.index >= len(self.text):
+                self.done = True
+
+    
