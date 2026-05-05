8578fed9d12de46debbe44c20f966545bbc45308
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-05 18:29:10 -0500

    defining the draw image

--- src/ui/portraits.py
+++ src/ui/portraits.py
@@ -4,3 +4,6 @@ class Portrait:
     def __init__(self, image_path, pos):
         self.image = pygame.image.load(image_path)
         self.pos = pos
+
+    def draw(self, screen):
+        screen.blit(self.image, self.pos)
