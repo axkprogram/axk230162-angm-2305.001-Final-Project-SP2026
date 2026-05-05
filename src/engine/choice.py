86cfed22d95856a08d1e099cdd932bbc4b936678
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-05 18:21:09 -0500

    defining the set choices in the Choice Manager class

--- src/engine/choice.py
+++ src/engine/choice.py
@@ -4,3 +4,11 @@ class ChoiceManager:
     def __init__(self, font):
         self.buttons = []
         self.font = font
+
+    def set_choices(self, choices):
+        self.buttons = []
+        y = 400
+        for text, action, in choices :
+            self. buttons.append(Button((100, y, 400, 50), text, action))
+            y += 70
+            
