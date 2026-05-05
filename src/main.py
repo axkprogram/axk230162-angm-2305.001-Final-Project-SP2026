a210d875de93adcd294eadf9d812dcf53fff074f
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-05 18:51:02 -0500

    finishing main

--- src/main.py
+++ src/main.py
@@ -9,3 +9,28 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 font = pygame.font.SysFont(None, 32)
+
+state = GameState()
+dialogue = DialogueManager(font)
+choices = ChoiceManager(font)
+
+dialogue.start("Welcome to your Visual Novel")
+
+running = True
+
+while running:
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        result = choices.handle_event(event)
+        if result:
+            print("Selected:", result)
+
+    dialogue.update()
+    dialogue.draw(screen)
+    choices.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(FPS)
