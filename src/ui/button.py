c87770dd950cb3ac5fb65c487fb9f8d2919c2f24
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-05 18:17:01 -0500

    defining the handle event in the button class and adding a return

--- src/ui/button.py
+++ src/ui/button.py
@@ -10,3 +10,8 @@ class Button:
         pygame.draw.rect(screen, (100,100,100), self.rect)
         txt = font.render(self.text, True (255,255,255))
         screen.blit(txt, (self.rect.x + 10,self.rect.y + 10))
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                return self.action
