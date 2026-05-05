03172e68aeb999e471ff257cdf558cab1df9269d
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-05 18:26:28 -0500

    creating and defining load_game variable

--- src/engine/save_load.py
+++ src/engine/save_load.py
@@ -3,3 +3,7 @@ import pickle
 def save_game(state, filename="save.dat"):
     with open(filename, "wb") as f:
         pickle.dump(state, f)
+
+def load_game(filename="save.dat"):
+    with open(filename, "rb") as f:
+        return pickle.load(f)
