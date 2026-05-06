3c04b39feae99efd2c97b6f0d27bed5ed2ea1469
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-05 20:10:26 -0500

    fixing the button

--- src/ui/button.py
+++ src/ui/button.py
@@ -6,12 +6,24 @@ class Button:
         self.text = text
         self.action = action
 
-    def draw(self, screen, font):
-        pygame.draw.rect(screen, (100,100,100), self.rect)
-        txt = font.render(self.text, True (255,255,255))
-        screen.blit(txt, (self.rect.x + 10,self.rect.y + 10))
+        self.font = pygame.font.SysFont(None, 28)
+        self.hovered = False
 
-    def handle_event(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if self.rect.collidepoint(event.pos):
-                return self.action
+    def draw(self, screen):
+        color = (80,80,80) if not self.hovered else (120,120,120)
+
+        pygame.draw.rect(screen, color, self.rect)
+        pygame.draw.rect(screen, (255,255,255), self.rect, 2)
+
+        text_surface = self.font.render(self.text, True, (255,255,255))
+        screen.blit(
+            text_surface
+            (self.rect.x +10, self.rect.y +10)
+        )
+
+    def update(self, mouse_pos):
+        self.hovered = self.rect.collidepoint(mouse_pos)
+
+    def click(self):
+        if self.hovered:
+            return self.action
