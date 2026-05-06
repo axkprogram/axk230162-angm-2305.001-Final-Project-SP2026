d2aef7c9d65e4e2e28f9ac1c64c2b29dc55041cf
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-05 19:46:24 -0500

    fixing the combat.py

--- src/engine/combat.py
+++ src/engine/combat.py
@@ -1,36 +1,50 @@
 # engine/combat.py
 import random
 
-class Combat:
+class CombatSystem:
     def __init__(self, state):
         self.state = state
         self.turn = 0
-        self.enemy_hp = 100
+        self.enemy_hp = 80
+
         self.used_dagger = False
         self.used_spell = False
+    
+    def start_enemy(self, enemy):
+        self.enemy_hp = 80
+        self.turn = 0
 
-    def player_attack(self, attack):
+    def player_action(self, action):
         self.turn += 1
 
-        if attack == "dagger":
+        if action == "dagger":
             self.used_dagger = True
             self.enemy_hp -= 10
 
-        elif attack == "spell":
+        elif action == "spell":
             self.used_spell = True
             self.enemy_hp -= 10
 
-        elif attack == "bell":
+        elif action == "bell":
             if not (self.used_dagger or self.used_spell):
-                return "Bell locked"
+                return "Locked"
             return "BELL_TRIGGER"
         
-        return "continue"
+        return "ok"
     
-    def enemy_attack(self):
-        return random.randint(5,15)
+    def enemy_action(self):
+        self.state.player_hp -= random.randint(5,10)
+        self.trun += 1
 
-    def check_annihiliation(self):
+    def update(self):
+        if self.enemy_hp <= 0:
+            return "WIN"
+        
+        if self.state.player_hp <= 0:
+            return "LOSE"
+        
         if self.turn >= 4:
-            return True
-        return False
+            return "ANNIHILATION"
+        
+        self.enemy_action()
+        return "CONTINUE"
