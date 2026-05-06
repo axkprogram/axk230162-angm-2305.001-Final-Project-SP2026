1ff8151e1d3822030e220dc453cc7db8f52a4897
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-05 19:26:14 -0500

    fixing the choice manager

--- src/engine/choice.py
+++ src/engine/choice.py
@@ -1,24 +1,13 @@
 from ui.button import Button
 
 class ChoiceManager:
-    def __init__(self, font):
-        self.buttons = []
-        self.font = font
+    def __init__(self):
+        self.choices = []
 
     def set_choices(self, choices):
-        self.buttons = []
-        y = 400
-        for text, action, in choices :
-            self. buttons.append(Button((100, y, 400, 50), text, action))
-            y += 70
+        self.choices = choices
 
-    def draw(self, screen):
-        for b in self.buttons:
-            b.draw(screen, self.font)
-
-    def handle_event(self, event):
-        for b in self.buttons:
-            result = b.handle_event(event)
-            if result:
-                return result
-            
+    def pick(self, index):
+        if 0 <= index < len(self.choices):
+            self.choices[index][1]()
+            self.choices = []
