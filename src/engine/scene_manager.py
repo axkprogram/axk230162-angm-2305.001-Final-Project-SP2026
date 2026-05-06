aaa66e4920293f9bd87121909b86d605dadedd9d
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-05 19:09:30 -0500

    coding the intro scene

--- src/engine/scene_manager.py
+++ src/engine/scene_manager.py
@@ -8,3 +8,35 @@ class SceneManager:
         self.combat = combat
 
         self.current_scene = "intro"
+
+    def update(self):
+        if self.current_scene == "intro":
+            self.intro_scene()
+
+        elif self.current_scene == "fork":
+            self.fork_scene()
+
+        elif self.current_scene == "fighter_path":
+            self.fighter_scene()
+
+        elif self.current_scene == "combat":
+            self.combat_scene()
+
+
+# Intro Scene
+
+def intro_scene(self):
+    self.dialogue([
+        "You fall into a cave beneath the earth.",
+        "Dust settles around you."
+    ])
+
+    self.choice.set_choices([
+        ("Stand up", self.to_fork)
+        ])
+    
+def to_fork(self):
+    self.current_scene = "fork"
+
+# Fork Scene
+
