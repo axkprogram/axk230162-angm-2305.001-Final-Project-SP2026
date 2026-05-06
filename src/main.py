3bc6c82e551834f217813a10c611536448d7ce11
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-05 20:27:00 -0500

    main update

--- src/main.py
+++ src/main.py
@@ -6,45 +6,82 @@ from engine.scene_manager import SceneManager
 from engine.dialogue import DialogueManager
 from engine.choice import ChoiceManager
 from engine.combat import CombatSystem
+from ui.textbox import Textbox
+from ui.ui_manager import UIManager
+
+# init
 
 pygame.init()
-screen = pygame.display.set_mode((1280,720))
+
+WIDTH, HEIGHT = 1280, 720
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Visual Novel Engine")
+
 clock = pygame.time.Clock()
 
-state = GameState()
-dialogue = DialogueManager()
-choice = ChoiceManager()
-combat = CombatSystem(state)
+# Core systems
 
-scene = SceneManager(state, dialogue, choice, combat)
+state = GameState
+dialogue = DialogueManager
+choices = ChoiceManager
+combat = CombatSystem
 
+scene = SceneManager(state, dialogue, choices, combat)
+
+# UI System
+
+textbox = Textbox(WIDTH, HEIGHT)
+ui = UIManager(textbox, choices)
+
+# Game Loop
 running = True
 
 while running:
     screen.fill((0,0,0))
 
+    mouse_pos = pygame.mouse.get_pos()
+
+    # Events
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        # Dialogue advance
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 dialogue.advance()
 
-            if event.key == pygame.K_1:
-                choice.pick(0)
-            if event.key == pygame.K_2:
-                choice.pick(1)
-            if event.key == pygame.K_3:
-                choice.pick(2)
+        # Choice selection
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for button in choices.choices:
+                if button in choices.choices:
+                    if button.rect.collidepoint(event.pos):
+                        result = button.action()
 
-    # Core loop
-    scene.update()
+                        # optional debug
+                        if result == "BELL TRIGGER":
+                            print("Bell event triggered")
 
-    font = pygame.font.SysFont(None, 32)
+# Game logic update
 
-    text = font.render(dialogue.get_current(), True, (255,255,255))
-    screen.blit(text, (50, 600))
+scene.update()
 
-    pygame.display.flip()
-    clock.tick(60)
+# Ui update
+
+ui.update(mouse_pos, choices.choices)
+
+# Draw UI
+
+current_text = dialogue.get_current()
+
+ui.draw (
+    screen, 
+    current_text,
+    choices.choices
+)
+
+# Flip screen
+
+pygame.display.flip()
+clock.tick(60)
