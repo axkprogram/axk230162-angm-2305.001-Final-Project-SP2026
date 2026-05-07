7b3961cf6b9613fc18f4d07d7c4bcd5cfd603f7e
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-07 16:22:08 -0500

    deleting

--- src/scenes/chapter1.py
+++
@@ -1,8 +0,0 @@
-def start_scene(dialogue, choice):
-    dialogue.start("You fall into a cave...")
-
-    return [
-        ("Follow Fighter", "fighter_path"),
-        ("Follow Mage", "mage_path"), 
-        ("Take Dark Path", "player_path")
-    ]
--- src/ui/portraits.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Portrait:
-    def __init__(self, image_path, pos):
-        self.image = pygame.image.load(image_path)
-        self.pos = pos
-
-    def draw(self, screen):
-        screen.blit(self.image, self.pos)
