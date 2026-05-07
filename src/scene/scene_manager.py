02a959046b6d96dc0a15e11a8dd8a541c304e7f5
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-07 18:10:11 -0500

    scene manager class defining and making internal __init__

---
+++ src/scene/scene_manager.py
@@ -0,0 +1,14 @@
+class SceneManager:
+    """
+    Handles scene execution (dialogue, choices, transitions).
+    
+    SceneManager does NOT control game flow.
+    It returns "results" to the EngineController.
+    """
+
+    def __init__(self):
+        self.current_scene = None
+        self.scene_data = None
+        self.node_index = 0
+
+        
