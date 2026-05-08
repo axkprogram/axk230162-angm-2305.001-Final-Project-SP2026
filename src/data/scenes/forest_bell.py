1711b1b616f5ee00750861cd1679e5dbd9681b68
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-08 09:05:06 -0500

    writing the forest bell, and the continuing in the forest part, starting the bell scene

---
+++ src/data/scenes/forest_bell.py
@@ -0,0 +1,109 @@
+forest_bell_scene = [
+
+    # Continue through the Forest
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "Narration",
+                "text": "The forest path narrows as the group continues deeper between the trees, until it disappears completely.", 
+                "emotion": "neutral"
+            },
+            {
+                "speaker": "Narration",
+                "text": "Roots twist through the dirt between Carmen's boots, forcing each step to stay measured.",
+                "emotion": "neutral"
+            },
+            {
+                "speaker": "Rio",
+                "text": "If this temple really exists, someone seriously picked the worst possible place to put it. Aren't temples usually reachable?",
+                "emotion": "casual"
+            },
+            {
+                "speaker": "Yohan",
+                "text": "It entirely depends on the practice of the temple. Isolation of religious structures isn't uncommon, it usually represents some kind of significance.",
+                "emotion": "analytical"
+            },
+            {
+                "speaker": "Carmen",
+                "text": "I don't think that's what Rio means... Or at least what I think.",
+                "emotion": "cautious"
+            },
+            {
+                "speaker": "Yohan",
+                "text": "Huh?",
+                "emotion": "curious"
+            },
+            {
+                "speaker": "Rio",
+                "text": "What's on your mind?",
+                "emotion": "curious"
+            },
+            {
+                "speaker": "Carmen",
+                "text": "Well... in both cases, the temple has an obvious presence. As well as an obvious figure of worship.",
+                "emotion": "honest"
+            },
+            {
+                "speaker": "Rio",
+                "text": "Yeah so?",
+                "emotion": "curious"
+            },
+            {
+                "speaker": "Carmen",
+                "text": "But there's been absolutely no sign that this temple even exists. And the noblewoman never mentioned a deity for the temple. Not even a name.",
+                "emotion": "concern"
+            },
+            {
+                "speaker": "Narration",
+                "text": "A realization dawns on everyone. Carmen was right. All they've seen is forest around them, even after exploring every inch of the place.",
+                "emotion": "neutral"
+            },
+            {
+                "speaker": "Narration",
+                "text": "Yohan shifts uncomfortably.",
+                "emotion": "netural"
+            },
+            {
+                "speaker": "Yohan",
+                "text": "That doesn't make sense though. This place is bathed in magic.",
+                "emotion": "adamant"
+            },
+            {
+                "speaker": "Rio",
+                "text": "Yeah, but magic forests aren't uncommon.",
+                "emotion": "concern"
+            },
+            {
+                "speaker": "Yohan",
+                "text": "Not with this much magic though! Besides I'm sure the noblewoman gave us a deity name, you two just forgot it.",
+                "emotion": "adamant"
+            },
+            {
+                "speaker": "Carmen",
+                "text": "Do you remember then?",
+                "emotion": "serious"
+            },
+            {
+                "speaker": "Narration",
+                "text": "An uncomfortable silence falls on the three. It's like the forest is waiting for an answer.",
+                "emotion": "neutral"
+            },
+            {
+                "speaker": "Yohan",
+                "text": "...There's too much magic to ignore it.",
+                "emotion": "quiet"
+            },
+            {
+                "speaker": "Rio",
+                "text": "So I guess the temple is just hiding from us then.",
+                "emotion": "sarcastic"
+            }
+        ]
+    },
+
+    # The Bell
+    {
+        
+    }
+]
