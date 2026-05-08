e7c5a0ce3b6e24223b482eeb0181cfb532c69ba5
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-08 18:45:04 -0500

    main test

--- src/main.py
+++ src/main.py
@@ -1,30 +1,11 @@
 # testing
 
-import pygame
+from engine.game_loop import GameLoop
 
 def main():
-    pygame.init()
+    game = GameLoop()
+    game.run()
 
-    screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption("VN RPG Test Window")
-
-    clock = pygame.time.Clock()
-    running = True
-    
-    while running:
-
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                running = False
-
-        # BG color
-        screen.fill((20,20,20))
-
-        pygame.display.flip()
-        clock.tick(60)
-
-    pygame.quit()
 
 if __name__ == "__main__":
     main()
