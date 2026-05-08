1a8687a8753ba7fef4cfacea37321c3ce3b6a128
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-07 21:00:04 -0500

    writing the forest intro scene (I'm a writer I lose track of what I'm writing and don't save often)

--- src/data/scenes/forest_intro.py
+++ src/data/scenes/forest_intro.py
@@ -28,8 +28,97 @@ forest_intro_scene = [
         "lines": [
             {
                 "speaker": "Narration",
-                "text"
+                "text": "Carmen moves with steady focus, maintaining pace while keeping an eye out for anything that looks remotely like what they're looking for.",
+                "emotion": "focused"
+            },
+            {
+                "speaker": "Narration",
+                "text": "At their side hangs a bell. It doesn't ring, but Carmen carries it anwyay. They don't carry it for the sound anyway.",
+                "emotion": "neutral"
+            },
+            {
+                "speaker": "Narration",
+                "text": "It was given to them by an old friend. Donny, or Donovan. He had a matching bell.",
+                "emotion": "reflective"
+            },
+            {
+                "speaker": "Narration",
+                "text": "He went on a solo adventure one day, promising to send letters to Carmen. But not a single letter ever arrived",
+                "emotion": "serious"
+            },
+            {
+                "speaker": "Narration",
+                "text": "Donovan never told Carmen where he was going, leaving Carmen with absolutely nothing. Except for the bell.",
+                "emotion": "serious"
+            },
+            {
+                "speaker": "Narration",
+                "text": "A bell that caught the attention of a passing noblewoman. Intrigued and convinced of... something, the noblewoman insisted that Carmen take this one request of hers.",
+                "emotion": "serious"
+            },
+            {
+                "speaker": "Narration",
+                "text": "In exchange, the noblewoman promised to assist in Carmen's search for Donovan in anyway possible. It seemed an innocent enough request on the surface. Find a temple, and bring the noblewoman an artifact to prove the temple exists.",
+                "emotion": "neutral"
             }
+        ]
+    },
+
+    #Party intro
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "Narration",
+                "text": "A sharp voice and strained laughter somewhere in further in front of them, remind Carmen that they weren't alone on this request.",
+                "emotion": "realization"
+            },
+            {
+                "speaker": "Rio",
+                "text": "I'm sureeee that your magic monocles are working juuuuuust fine Mr. Tower Mage. I'm just saying that maybe we should rely on our own two eyes instead. You might've missed something.",
+                "emotion": "passive aggressive"
+            },
+            {
+                "speaker": "Narration",
+                "text": "Rio was a trained fighter. A popular fighter in the captial city. There's a confidence and readiness in her posture that is unmatched. Rio also had a wickedly handsome smile. Even if she didn't have the friendliest of intentions",
+                "emotion": "neutral"
+            },
+            {
+                "speaker": "Yohan",
+                "text": "You speak very confidently for someone who hasn't found anything either you know, Madame pretty fists. At least I'm trying to trace some kind of magic that resembles a holy temple. It's more usefully than playing with a sword.",
+                "emotion": "retaliation"
+            },
+            {
+                "speaker": "Narration",
+                "text": "Yohan was a prodigy mage from the capital city's Mage Tower. Every action he takes is calculated and purposeful. While Yohan may not have the sweetest personality, he was incredibly pretty for a seemingly overworked mage.",
+                "emotion": "neutral"
+            },
+            {
+                "speaker": "Rio",
+                "text": "You think way too much. I know it's in you mages' nature, but sometimes you just need to move forward.",
+                "emotion": "direct"
+            },
+            {
+                "speaker": "Yohan",
+                "text": "And you don't think enough. Leaping with fists flying doesn't necessarily lead to success",
+                "emotion": "calm rebuttal"
+            },
+            {
+                "speaker": "Narration",
+                "text": "Carmen sighed somewhat amused by Yohan and Rio's disagreement. The two of them didn't necessarily get along, the two's methods often clashing with each other, causing petty arguments like this. But they both knew they had a common goal. That said...",
+                "emotion": "netural"
+            },
+            {
+                "speaker": "Narration",
+                "text": "Yohan and Rio turn sharply towards Carmen, startling Carmen. The two have a look in their eyes that causes Carmen to take a hesitant step back and lift up their hands in mock surrender.",
+                "emotion": "neutral"
+            },
+            {
+                "speaker": "Carmen",
+                "text": "W-what is it? Why are you looking at me like that?",
+                "emotion": "nervous"
+            },
+            {}
         ]
     }
 ]
