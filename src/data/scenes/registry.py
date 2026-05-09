b65ac66d70f201a8534fe29099ab12ace08ae104
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-08 23:33:28 -0500

    updating the scene registry for all current scene scripts

--- src/data/scenes/registry.py
+++ src/data/scenes/registry.py
@@ -1,5 +1,18 @@
+
+"""
+Central scene registry.
+
+Maps scene IDs to scene data so the EngineController
+can transition scenes dynamically.
+"""
 from data.scenes.forest_intro import forest_intro_scene
+from data.scenes.forest_bell import forest_bell_scene
+from data.scenes.cave_fall import cave_fall_scene
+from data.scenes.deep_tunnel import  deep_tunnel_scene
 
 SCENE_REGISTRY = {
     "forest_intro": forest_intro_scene,
+    "forest_bell": forest_bell_scene,
+    "cave_fall": cave_fall_scene,
+    "deep_tunnel": deep_tunnel_scene
 }
