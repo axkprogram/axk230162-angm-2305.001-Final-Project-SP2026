755c9c39d800f425839bc8bf0913080063f56f78
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-09 14:26:03 -0500

    fork in tunnel scene script placeholder

---
+++ src/data/scenes/fork_in_tunnel.py
@@ -0,0 +1,14 @@
+fork_in_tunnel_scene = [
+    
+    #Place holder
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "Narration",
+                "text": "fork in tunnel placeholder",
+                "emotion": "neutral"
+            }
+        ]
+    }
+]
