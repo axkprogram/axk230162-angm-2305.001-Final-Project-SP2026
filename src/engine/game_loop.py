fad33914f39dfd067e50ddcb1fe16fd7521d65eb
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-08 22:40:34 -0500

    starting the gameloop init

--- src/engine/game_loop.py
+++ src/engine/game_loop.py
@@ -5,14 +5,23 @@ from state.game_state import GameState
 from scene.scene_manager import SceneManager
 from data.scenes.forest_intro import forest_intro_scene
 
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+
 class GameLoop:
     """
     Minimal Pygame loop for VN MVP.
     """
-
     def __init__(self):
         pygame.init()
 
-        # Window
-        self.screen = pygame.display.set_mode((800,600))
-        pygame
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("VN MVP")
+
+        self.clock = pygame.time.Clock()
+
+        self.font = pygame.font.SysFont("arial", 28)
+        self.small_font = pygame.font.SysFont("arial", 22)
+
+        
