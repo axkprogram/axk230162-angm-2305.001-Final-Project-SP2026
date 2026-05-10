de7293e8344d514a884cc748042c8ac97822747e
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-10 00:16:13 -0500

    finishing the sight leave

--- src/data/scenes/fighter/fighter_sight_leave.py
+++ src/data/scenes/fighter/fighter_sight_leave.py
@@ -1,1 +1,97 @@
-fighter_sight_leave_scene = []
+fighter_sight_leave_scene = [
+    
+    #decision to leave
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "Rio",
+                "text": "So... we're really leaving?"
+            },
+            {
+                "speaker": "Rio",
+                "text": "So we're really leaving?"
+            },
+            {
+                "speaker": "Yohan",
+                "text": "We found an artifact. That's what the noblewoman requested."
+            },
+            {
+                "speaker": "Rio",
+                "text": "Barely."
+            },
+            {
+                "speaker": "Carmen",
+                "text": "It's the safest choice."
+            }
+        ]
+    },
+
+    # character reactions
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "Yohan",
+                "text": "It is unfortunate. I would have liked to study this place longer."
+            },
+            {
+                "speaker": "Rio",
+                "text": "And I would've liked to know what was behind that door."
+            },
+            {
+                "speaker": "Narration",
+                "text": "Carmen says nothing."
+            },
+            {
+                "speaker": "Narration",
+                "text": "But they wonder if maybe who they're looking for might be somewhere beyond the door."
+            }
+        ]
+    },
+
+    #leaving
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "Narration",
+                "text": "The three turn toward the staircase."
+            },
+            {
+                "speaker": "Narration",
+                "text": "Each step upward feels heavier than the last."
+            },
+            {
+                "speaker": "Narration",
+                "text": "The cave begins to hum."
+            }
+        ]
+    },
+
+    #omnious ending
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "Narration",
+                "text": "For a moment, it feels like something deep within the cave is watching them."
+            },
+            {
+                "speaker": "Narration",
+                "text": "Laughing at them."
+            }
+        ]
+    },
+
+    #chapter end
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "System",
+                "text": "Chapter Complete: The Sight Wing"
+            }
+        ]
+    }
+]
