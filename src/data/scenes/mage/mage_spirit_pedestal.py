fa7e5e103b9b630d846d014dc02b7fa701937168
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-10 12:45:57 -0500

    spirit pedestal finsih

--- src/data/scenes/mage/mage_spirit_pedestal.py
+++ src/data/scenes/mage/mage_spirit_pedestal.py
@@ -1,1 +1,75 @@
-mage_spirit_pedestal_scene = []
+mage_spirit_pedestal_scene = [
+
+    # pedestal description
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "Narration",
+                "text": "The pedestal stands at the center of the chamber, worn smooth by time."
+            },
+            {
+                "speaker": "Narration",
+                "text": "A circular compartment has been carved into its center."
+            },
+            {
+                "speaker": "Yohan",
+                "text": "This wasn't decorative. Somehitng belongs here."
+            },
+            {
+                "speaker": "Rio",
+                "text": "Well do we have anything?"
+            }
+        ]
+    },
+
+    # player has talisman
+    {
+        "type": "conditional",
+        "if": {
+            "has_spirit_talisman": True
+        },
+        "true": [
+            {
+                "type": "choice",
+                "options": [
+                    {
+                        "text": "Place the Talisman of Spirit",
+                        "result": {
+                            "action": "change_scene",
+                            "target": "mage_spirit_open"
+                        }
+                    },
+                    {
+                        "text": "Leave the pedestal alone",
+                        "result": {
+                            "action": "change_scene",
+                            "target": "mage_spirit_hub_menu"
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    # default if player doesn't have talisman
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "Carmen",
+                "text": "We don't have anything that fits."
+            },
+            {
+                "speaker": "Yohan",
+                "text": "Then our answer must be elsewhere."
+            }
+        ]
+    },
+
+    #transition
+    {
+        "type": "transition",
+        "target_scene": "mage_spirit_hub_menu"
+    }
+]
