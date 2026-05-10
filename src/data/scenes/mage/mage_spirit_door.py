c33627486be8bc625fbe60eccbac8b26a7b97a11
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-10 12:55:47 -0500

    finsih the scene script

--- src/data/scenes/mage/mage_spirit_door.py
+++ src/data/scenes/mage/mage_spirit_door.py
@@ -1,1 +1,77 @@
-mage_spirit_door_scene = []
+mage_spirit_door_scene = [
+
+    # door desc
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "Narration",
+                "text": "The stone door towers above the party."
+            },
+            {
+                "speaker": "Narration",
+                "text": "A specter is carved into its surface, surrounded by blue flames."
+            },
+            {
+                "speaker": "Narration",
+                "text": "At the center of its chest is an empty circular socket."
+            },
+            {
+                "speaker": "Yohan",
+                "text": "Its core is missing."
+            },
+            {
+                "speaker": "Rio",
+                "text": "Which means we're probably carrying the key"
+            }
+        ]
+    },
+
+    # player has gemstone
+    {
+        "type": "conditional",
+        "if": {
+            "has_spirit_gem": True
+        },
+        "true": [
+            {
+                "type": "choice",
+                "options": [
+                    {
+                        "text": "Place the blue gemstone into the missing core",
+                        "result": {
+                            "action": "change_scene",
+                            "target": "mage_spirit_open"
+                        }
+                    },
+                    {
+                        "text": "Leave the door alone",
+                        "result": {
+                            "action": "change_scene",
+                            "target": "mage_spirit_hub_scene"
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    # default if no gemstone
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "Carmen",
+                "text": "We don't have what belongs here."
+            },
+            {
+                "speaker": "Yohan",
+                "text": "Then we keep searching."
+            }
+        ]
+    },
+    {
+        "type": "transition",
+        "target_scene": "mage_spirit_hub_menu"
+    }
+]
