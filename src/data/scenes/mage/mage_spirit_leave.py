1768eed781eee57d3831af2fc73782e0eb672271
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-10 13:27:19 -0500

    scene script finish

--- src/data/scenes/mage/mage_spirit_leave.py
+++ src/data/scenes/mage/mage_spirit_leave.py
@@ -1,1 +1,76 @@
-mage_spirit_leave_scene = []
+mage_spirit_leave_scene = [
+
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "Rio",
+                "text": "We have an artifact. That's what we came here for."
+            },
+            {
+                "speaker": "Rio",
+                "text": "No reason to throw ourselves at whatever this is."
+            },
+            {
+                "speaker": "Yohan",
+                "text": "That is an extremely good reason to throw ourselves at whatever this is."
+            },
+            {
+                "speaker": "Carmen",
+                "text": "It would be safer to leave."
+            }
+        ]
+    },
+
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "Yohan",
+                "text": "It's painful handing something historically significant to some noble collector."
+            },
+            {
+                "speaker": "Rio",
+                "text": "You'll survive."
+            },
+            {
+                "speaker": "Narration",
+                "text": "Carmen glances once more toward the sealed door."
+            },
+            {
+                "speaker": "Narration",
+                "text": "Thye wonder what-- or who-- might be waiting beyond it."
+            }
+        ]
+    },
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "Narration",
+                "text": "The cave hums softly as they climb the stairs."
+            },
+            {
+                "speaker": "Narration",
+                "text": "Carmen feels it again."
+            },
+            {
+                "speaker": "Narration",
+                "text": "Something unseen. Watching."
+            },
+            {
+                "speaker": "Narration",
+                "text": "Laughing."
+            }
+        ]
+    },
+    {
+        "type": "dialogue",
+        "lines": [
+            {
+                "speaker": "System",
+                "text": "Chapter Complete: The Spirit Wing"
+            }
+        ]
+    }
+]
