d507dcc351dc8885a84d331063b3a79ad0192980
Akshitha Kodimela <axk230162@utdallas.edu>
2026-05-10 12:37:29 -0500

    hub menu finish

--- src/data/scenes/mage/mage_spirit_hub_menu.py
+++ src/data/scenes/mage/mage_spirit_hub_menu.py
@@ -1,1 +1,81 @@
-mage_spirit_hub_menu_scene = []
+mage_spirit_hub_menu_scene = [
+
+    #if player has the spirit bell show all four choices
+    {
+        "type": "conditional",
+        "if": {
+            "has_spirit_bell": True
+        },
+        "true": [
+            {
+                "type": "choice",
+                "option": [
+                    {
+                        "text": "Investigate the pedestal",
+                        "result": {
+                            "action": "change_scene",
+                            "target": "mage_spirit_pedestal"
+                        }
+                    },
+                    {
+                        "text": "Investigate the door",
+                        "result": {
+                            "action": "change_scene",
+                            "target": "mage_spirit_door"
+                        }
+                    },
+                    {
+                        "text": "Ring the Bell",
+                        "result": {
+                            "action": "change_scene",
+                            "target": "mage_spirit_bell"
+                        }
+                    },
+                    {
+                        "text": "Leave up the stairs",
+                        "result": {
+                            "action": "change_scene",
+                            "target": "mage_spirit_leave"
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    # default if no bell
+    {
+        "type": "conditional",
+        "if": {
+            "has_spirit_bell": True
+        },
+        "true": [
+            {
+                "type": "choice",
+                "option": [
+                    {
+                        "text": "Investigate the pedestal",
+                        "result": {
+                            "action": "change_scene",
+                            "target": "mage_spirit_pedestal"
+                        }
+                    },
+                    {
+                        "text": "Investigate the door",
+                        "result": {
+                            "action": "change_scene",
+                            "target": "mage_spirit_door"
+                        }
+                    },
+                    {
+                        "text": "Leave up the stairs",
+                        "result": {
+                            "action": "change_scene",
+                            "target": "mage_spirit_leave"
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+]
